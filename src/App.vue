5acadee0bb6b2530844c2ccc3af7732e6ed859a9
Dani Barros <daniel.barroslm@gmail.com>
2020-12-10 23:57:31 -0300

    iniciando com componentes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ButtonVue></ButtonVue>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import ButtonVue from './components/ButtonVue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-  },
+    ButtonVue,
+  }
 };
 </script>
 
 <style lang="scss">
-#app {
+#root {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
